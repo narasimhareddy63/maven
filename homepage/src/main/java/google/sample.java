c20e53830f8a74fc7142953821a62d5809ea2435
narasimhareddy63 <narasimhareddy7288@gmail.com>
2024-10-18 05:27:27 +0000

    homepage

---
+++ homepage/src/main/java/google/sample.java
@@ -0,0 +1,13 @@
+package google;
+
+/**
+ * Hello world!
+ *
+ */
+public class sample
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World123!" );
+    }
+}
